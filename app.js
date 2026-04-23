0cc5b05c61922e6a9923ab90d48db718a26054e3
janainaestudos584-ctrl <janainaestudos584@gmail.com>
2026-04-22 21:41:34 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -22,4 +22,73 @@ document.getElementById('formCandidato').addEventListener('submit', function(eve
 
     alert(`${nome} cadastrado com sucesso!`);
     this.reset(); // Limpa o formulário
+    console.log(bancoDeCandidatos)
 });
+
+function listarTodos() {
+    const divResultado = document.getElementById('resultado');
+    
+    if (bancoDeCandidatos.length === 0) {
+        divResultado.innerHTML = "<p>Nenhum candidato cadastrado.</p>";
+        return;
+    }
+
+    // 🚀 TRANSFORMANDO DADOS EM HTML COM .map()
+    const htmlCandidatos = bancoDeCandidatos.map(candidato => {
+        return `
+            <div class="card">
+                <strong>Nome:</strong> ${candidato.nome} <br>
+                <strong>Stack:</strong> ${candidato.tecnologia} <br>
+                <strong>XP:</strong> ${candidato.experiencia} anos
+            </div>
+        `;
+    });
+
+    // O .join('') tira as vírgulas que separam os itens do array
+    divResultado.innerHTML = htmlCandidatos.join(''); 
+}
+
+//filtrando os sêniores
+function filtrarSeniores() {
+    // 🚀 FILTRANDO DADOS COM .filter()
+    const seniores = bancoDeCandidatos.filter(candidato => candidato.experiencia >= 5);
+
+    const divResultado = document.getElementById('resultado');
+    
+    if (seniores.length === 0) {
+        divResultado.innerHTML = "<p>Nenhum candidato Sênior encontrado.</p>";
+        return;
+    }
+
+    // Reutilizamos o map para exibir o novo array filtrado
+    const htmlSeniores = seniores.map(candidato => {
+        return `<div class="card" style="border-color: green;">
+                    🌟 <strong>${candidato.nome}</strong> (${candidato.experiencia} anos) - ${candidato.tecnologia}
+                </div>`;
+    });
+
+    divResultado.innerHTML = htmlSeniores.join('');
+}
+// calcular mediaxp
+function calcularMediaXP() {
+    const divResultado = document.getElementById('resultado');
+
+    if (bancoDeCandidatos.length === 0) {
+        divResultado.innerHTML = "<p>Cadastre candidatos para calcular a média.</p>";
+        return;
+    }
+
+    // 🚀 REDUZINDO O ARRAY A UM ÚNICO VALOR COM .reduce()
+    // O acumulador (total) começa com 0, e vamos somando a XP de cada candidato
+    const totalXP = bancoDeCandidatos.reduce((acumulador, candidato) => {
+        return acumulador + candidato.experiencia;
+    }, 0);
+
+    const media = totalXP / bancoDeCandidatos.length;
+
+    divResultado.innerHTML = `
+        <div class="card" style="background-color: #f0f8ff;">
+            <strong>Média de Experiência da Base:</strong> ${media.toFixed(1)} anos.
+        </div>
+    `;
+}
